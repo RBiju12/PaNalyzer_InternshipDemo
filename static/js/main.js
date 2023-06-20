e9b335c15fc398d4410b37c8db6bc07063b764a3
Rishan Biju <rishanbiju@gmail.com>
2023-06-20 13:55:46 -0400

    Added Test Cases and Validation

--- static/js/main.js
+++ static/js/main.js
@@ -4,13 +4,36 @@ async function getFileType(){
 
         const filecontent = await fileGrab.getFile();
 
+        //const versionumber = filecontent.
+
         const filetype = filecontent.name.split(".").pop();
+        
+        const [testfile, ...resttestfile] = filecontent.name.split("-");
+
+        const present = 2023
+
+        const url = "\nhttps://www.anaconda.com/download"
 
         //filetype access .name from the console
+        if(filetype === "exe"){
+            if(resttestfile[0] < present){
+                alert("You need to download the Latest version of Ananaconda: ".concat(url));
+            }
+            else{
 
-        if(filetype !== "exe" || filetype === null){
-            alert("Filetype must be .exe");
+                alert("You already meet the latest version requirements");
+            }
         }
+
+        else if(filetype !== "exe" || filetype === null){
+            alert("Filetype must be .exe");
+            console.log(filecontent)
+        }
+
+        else{
+            alert('File is not compatible')
+        }
+
     }
     catch(error){
         console.log(`Your error is: ${error}`)
